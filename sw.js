c5948b5b671fc015329839fd495e161995733f5f
wosvip <wellington_wos@ymail.com>
2026-08-22 06:17:45 -0400

    Atualizar cache do aplicativo instalável

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='wosvip-calculadora-v2';
-const ASSETS=['./','./index.html','./styles.css?v=7','./app.js?v=6','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
+const CACHE='wosvip-calculadora-v3';
+const ASSETS=['./','./index.html','./styles.css?v=7','./app.js?v=6','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))))});
